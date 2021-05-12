f3856428e32b7f49b3a7b8d5c3f831a720b6bbfb
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-05-12 14:10:27 +0200

    New vers

---
+++ DeleteClassFiles.py
@@ -0,0 +1,58 @@
+import os
+from os import listdir
+from os.path import isfile, join
+#folder_to_track = input('folder to track :')
+folder_to_track = r'\Users\Utilisateur\Documents'
+folder_to_track.replace('\\',"/")
+
+files = [f for f in listdir(folder_to_track) if isfile(join(folder_to_track, f))]
+print(files)
+
+directories = [ name for name in os.listdir(folder_to_track) if os.path.isdir(os.path.join(folder_to_track, name)) ]
+if '.tmp.drivedownload' in directories :
+    directories.remove('.tmp.drivedownload')
+if 'Mes vidéos' in directories :
+    directories.remove('Mes vidéos')
+if 'Ma musique' in directories :
+    directories.remove('Ma musique')
+if 'Mes images' in directories :
+    directories.remove('Mes images')
+i = len(directories) - 1
+while i >= 0  :
+    directories[i] = folder_to_track + '/' + directories[i]
+    i = i - 1
+    print(directories[i])
+print(directories)
+
+for filename in files :
+    list_filename = filename.split('.')
+    if list_filename[-1] == 'class':
+        print('>>',folder_to_track + '/' + filename)
+        file_to_delete = folder_to_track + "/" + filename
+        os.remove(file_to_delete)
+        print('> deleted')
+directoriesToCheck = []
+i = 0
+while len(directories) > 0 :
+    files = [f for f in listdir(directories[i]) if isfile(join(directories[i], f))]
+    directoriesInDirectories = [ name for name in os.listdir(directories[i]) if os.path.isdir(os.path.join(directories[i], name)) ]
+    print('>>>>',directories[i])
+    if len(directoriesInDirectories) > 0 :
+        j = 0
+        while j < len(directoriesInDirectories)  :
+            directoriesToCheck.append(directories[i] + '/' + directoriesInDirectories[j])
+            print('-+-+',directoriesToCheck[j])
+            j = j + 1
+    print(files)
+    for filename in files :
+        list_filename = filename.split('.')
+        if list_filename[-1] == 'class':
+            print('>>',directories[i] + '/' + filename)
+            file_to_delete = directories[i] + "/" + filename
+            os.remove(file_to_delete)
+            print('> deleted')
+    del directories[i]
+    print(directories)
+    if len(directories) == 0 :
+        directories = directoriesToCheck
+        directoriesToCheck = []
